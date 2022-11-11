51d2608b5c692b5206758f2ea4b91aa11bd23c8e
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-11-11 13:33:23 -0300

    refactor: replace consts

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,22 @@
 import 'package:agenda/models/mission.dart';
 
+const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro'
+];
+
+const weekdays = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'];
+
 final allEvents = [
   Mission(ministry: 'Música', dateTime: DateTime.now(), missionaries: []),
 ];
