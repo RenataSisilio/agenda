d68f372f614e000a673c852ee08e804e8680a8f0
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-12-12 14:43:59 -0300

    feat: add user name to database

--- lib/auth/auth_controller.dart
+++ lib/auth/auth_controller.dart
@@ -1,8 +1,10 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
+import '../paths.dart';
 import 'app_auth_state.dart';
 
 class AuthController extends Cubit<AppAuthState> {
@@ -14,10 +16,17 @@ class AuthController extends Cubit<AppAuthState> {
     FirebaseUIAuth.configureProviders([
       EmailAuthProvider(),
     ]);
+    final userCollection =
+        FirebaseFirestore.instance.collection(Paths.userCollection);
+    final database = await userCollection.get();
 
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
         emit(LoggedAuthState());
+        if (database.docs.indexWhere((element) => element.id == user.uid) ==
+            -1) {
+          userCollection.doc(user.uid).set({'name': user.displayName});
+        }
       } else {
         emit(NotLoggedAuthState());
       }
